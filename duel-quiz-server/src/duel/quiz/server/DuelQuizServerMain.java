0ef72c8ca97c6714457d9b8a259ca322e69844d6
corteshs <sacortesh@gmail.com>
2014-11-24 14:42:02 +0100

    Commit de structure

---
+++ duel-quiz-server/src/duel/quiz/server/DuelQuizServerMain.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package duel.quiz.server;
+
+/**
+ *
+ * @author corteshs
+ */
+public class DuelQuizServerMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
