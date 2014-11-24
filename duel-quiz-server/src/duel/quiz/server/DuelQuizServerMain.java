103a0e0fe0d0918becdff47ee8a229751572e25a
corteshs <sacortesh@gmail.com>
2014-11-24 15:52:48 +0100

    message treating

--- duel-quiz-server/src/duel/quiz/server/DuelQuizServerMain.java
+++ duel-quiz-server/src/duel/quiz/server/DuelQuizServerMain.java
@@ -17,57 +17,54 @@ public class DuelQuizServerMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
 
-        //Puerto que se abrirá en esta máquina para responder a los clientes
-        int PUERTO_SQRT = 5001;
+        //Port to listen the clients
+        int PORT_LISTENER = 5001;
 
         Socket socket = null;
         ServerSocket serverSocket = null;
 
         try {
-            serverSocket = new ServerSocket(PUERTO_SQRT);
+            serverSocket = new ServerSocket(PORT_LISTENER);
         } catch (Exception e) {
             System.err.println("Error");
             return;
         }
-        //Mientras el programa esté funcionando
+        //Listening while running
         while (true) {
             try {
                 System.out.println("Ready to receive connections...");
-                //Acepto una petición de conexión
+                //Accept a connection
                 socket = serverSocket.accept();
 
-                //Creo dos canales de comunicación con la conexión
+                //Creates two streams of data
                 DataOutputStream out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
                 DataInputStream in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
 
                 System.out.println("Welcome");
-                //System.out.print("Esperando valor... ");
 
-                //Recibo el valor desde el cliente
+                //Recieving protocol message from client
                 String message = in.readUTF();
 
-                
+                //Business logic
                 String response = treatMessage(message);
-                
-                System.out.print("Response..." + response);
-                //Realizo la operación
 
-                //La pongo en el canal de salida
+                //Response
+                System.out.print("Response..." + response);
+
+                //Put on out channel (to client)
                 out.writeUTF(response);
                 System.out.print("Sending response... ");
-                //Se envía
                 out.flush();
                 System.out.println("OK");
 
-                //Se cierra la petición de conexión llamada socket
+                //Closing
                 try {
                     socket.close();
                 } catch (IOException ex) {
                 }
             } catch (Exception e) {
-                System.err.println("Cerrando la conexion del socket ...");
+                System.err.println("Closing...");
                 if (socket != null) {
                     try {
                         socket.close();
@@ -79,27 +76,44 @@ public class DuelQuizServerMain {
     }
 
     private static String treatMessage(String message) {
-        
+
         //Method to treat the incoming messages.
-        
+
         String output = "";
-        
-        switch (message){
+
+        switch (message) {
             case "LOGIN":
-                String user = "user"; //Obtain user (from message or message)
+                String user = "user"; //Obtain user (from message or protocol)
                 String pass = "pass"; //Obtain pass
-                
+
+                output = loginUser(user, pass);
+
                 //TODO Construct a model for the user
                 //TODO Set Variables in DAO
                 break;
+                
+            case "REGISTER":
+                break;
             default:
+                //the message is not compliant with any other message
                 output = "ERROR1";
                 break;
-               
+
         }
-        
+
         return output;
-        
+
+    }
+
+    private static String askForData(String dataHint) {
+
+        return "";
+
+    }
+
+    private static String loginUser(String user, String pass) {
+        //TODO Create a something and act on it
+
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
-
